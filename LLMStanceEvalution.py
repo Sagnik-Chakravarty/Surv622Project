c0b5a0b75beb430fe1e965dfdff858b388e3994c
Asuka-nn <yuchennn@umich.edu>
2025-04-07 01:54:13 -0400

    Add files via upload

--- LLMStanceEvalution.py
+++ LLMStanceEvalution.py
@@ -1,16 +1,26 @@
 import pandas as pd
+import numpy as np
+
 from sklearn.metrics import accuracy_score, precision_score, recall_score
 
 # read data
 df = pd.read_csv("/Users/asuka/Downloads/LLM_Stance.csv")
 
-# true label
-y_true = df['Sentiment Full'].astype(str).str.strip()
+# set ramdom seed
+np.random.seed(123)
+
+# get train set
+train_idx = np.random.choice(df.index, size=int(0.8 * len(df)), replace=False)
+test_idx = df.index.difference(train_idx)
+df_test = df.loc[test_idx]
+
 
 # clean prediction format
-svm_pred = df['SVM_Pred'].astype(str).str.strip()
-tree_pred = df['Tree_Pred'].astype(str).str.strip()
-llm_pred = df['stance_predictions'].astype(str).str.strip()
+y_true = df_test['Sentiment Full'].astype(str).str.strip()
+svm_pred = df_test['SVM_Pred'].astype(str).str.strip()
+tree_pred = df_test['Tree_Pred'].astype(str).str.strip()
+llm_pred = df_test['stance_predictions'].astype(str).str.strip()
+
 
 # evalution
 def evaluate_model(name, y_pred):
@@ -18,6 +28,7 @@ def evaluate_model(name, y_pred):
     print("Accuracy:", accuracy_score(y_true, y_pred))
     print("Precision:", precision_score(y_true, y_pred, average='macro'))
     print("Recall:", recall_score(y_true, y_pred, average='macro'))
+
 
 evaluate_model("SVM Model", svm_pred)
 evaluate_model("Decision Tree Model", tree_pred)
